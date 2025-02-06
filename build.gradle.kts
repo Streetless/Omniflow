a9ae8beab0cfd07af381e06a4c34c7e2e2a869af
Alwyn974 <alwyn974@gmail.com>
2025-02-06 22:31:00 +0100

    build(deps): Update kotlin to v2.1.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import java.net.URI
 
 plugins {
     application
-    kotlin("jvm") version "2.1.0"
+    kotlin("jvm") version "2.1.10"
     id("com.github.johnrengelman.shadow") version "7.1.2"
     kotlin("plugin.serialization") version "2.1.0"
     id("maven-publish")
